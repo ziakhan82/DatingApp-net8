988f1a87c519b0ebc21346d2a968ab49b81a212b
ziakhan82 <ziaullahhassan8@gmail.com>
2024-12-08 08:51:11 +0100

    Refactor code to use async/await for improved performance

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+        public DbSet<AppUser> Users  { get; set; }
+    }
+
